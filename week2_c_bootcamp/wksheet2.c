11678091f4cdb63c5e565437cff673798abdfb5f
adambaig1 <146704935+adambaig1@users.noreply.github.com>
2023-10-13 12:34:41 +0000

    commit msg

--- week2_c_bootcamp/wksheet2.c
+++ week2_c_bootcamp/wksheet2.c
@@ -7,16 +7,24 @@ int main(){
     int sum = 0;
     int length = 0;
 
-    while(arr[length] != "\0"){
-        length++;
-    }
-
-    for (int i = 0; i<length ; i++){
+    for (int i = 0; i<5 ; i++){
         sum = sum + arr[i];
     }
     printf("the sum of the array is %d\n", sum);
 
     //2
+    int arr2[5];
+    int c;
+    for (int i = 5; i>=0; i--){
+        c = arr[i];
+        for (int z = 0; z<5; z++){
+            arr2[z]=c;
+        }
+    }
+
+    for (int r = 5; r<5; r++){
+        printf("%d\n", arr2[r]);
+    }
     
 
 
