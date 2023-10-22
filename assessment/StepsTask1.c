8396b757dc6ca71ac681b6a92833dd0ce2dc1c48
adambaig1 <146704935+adambaig1@users.noreply.github.com>
2023-10-22 12:04:25 +0000

    part 1 finally doneeeeee :)

--- assessment/StepsTask1.c
+++ assessment/StepsTask1.c
@@ -43,55 +43,58 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 // Complete the main function
 int main() {
+    //this initializes the file pointer and sets it to the csv file that is to be read
     FILE *fp;
-    fp = fopen("Fitness_Data.csv", "r");
+    fp = fopen("FitnessData_2023.csv", "r");
 
+    //this prints out an error message if the file cannot be opened
     if (fp == NULL){
         printf("unable to open file");
     } 
 
+    //count is the variable used to count the records in the file, current_c is used to determine when the end of a record is
     int count = 1;
     char current_c;
-
+    //this while loop iterates until the end of the file, it uses the fgetc function which goes through each character in the csv
     while (current_c != EOF){
         current_c = fgetc(fp);
+        //if there is a newline character thats when the counter is incremented, signifying another record
         if (current_c == '\n'){
             count++;
         }       
     }
-
+    //this prints out the number of records in the file
     printf("Number of records in file: %d\n", count);
-
-    FITNESS_DATA reccords[count];
-
-    char mystr[27];
-
-    int c= 0;
-    while(c<2){
+    //this initializes a typedef array holding all the records, with the size beign variable to the size of the csv file
+    FITNESS_DATA records[count];
+    //I had to create a new file pointer as the data it was collecting was corrupting
+    FILE *fptr;
+    fptr = fopen("FitnessData_2023.csv", "r");
+    //this initializes the string which is used to store the records in the csv
+    char str[27];
+    //this for loop starts from 0 to the length of the file
+    //it gets the line of the file via the fegts function, then uses the tokenisRecord function to create tokens
+    //these tokens are then stored in the records array
+    for(int i = 0; i<=count;i++){
         char date[11];
         char time[6];
-        char steps[10];
-        fgets(mystr, 27, fp);
-        tokeniseRecord(mystr, ",", date, time, steps);
-
-        printf("%s\n", date);
-        printf("%s\n", time);
-        printf("%s\n", steps);
-
-        strcpy(reccords[c].date, date);
-        strcpy(reccords[c].time, time);
-        reccords[c].steps = atoi(steps);
-        c++;
+        char steps[5];
         
+        fgets(str, 27, fptr);
+        tokeniseRecord(str, ",", date, time, steps);
+
+        strcpy(records[i].date, date);
+        strcpy(records[i].time, time);
+        records[i].steps = atoi(steps);
+
+    }
+    //this for loop prints out the first three stored reccords in the array
+    for (int x = 0; x<3; x++){
+        printf("%s/%s/%d\n", records[x].date, records[x].time, records[x].steps);
     }
 
-
-
-    for(int c = 0; c<3; c++){
-        printf("%s/%s/%d\n",reccords[c].date ,reccords[c].time ,reccords[c].steps);
-
-    }
-
+    //this closes both filepointers
+    fclose(fptr);    
     fclose(fp);
     return 0;
 }
